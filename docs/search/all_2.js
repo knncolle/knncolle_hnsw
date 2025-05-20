597cfb4bfcba83da0b15fe65b495690f76735dfb
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2025-05-20 19:43:24 +0000

    Deploying to gh-pages from @ knncolle/knncolle_hnsw@4fc2a39265dcc5bc6b3b97a0217a6dde1c2de857 🚀

--- docs/search/all_2.js
+++ docs/search/all_2.js
@@ -2,9 +2,10 @@ var searchData=
 [
   ['can_5fsearch_5fall_0',['can_search_all',['https://knncolle.github.io/knncolle/classknncolle_1_1BruteforceSearcher.html#a7ae415a6fa177b498359371211c007a1',1,'knncolle::BruteforceSearcher::can_search_all()'],['https://knncolle.github.io/knncolle/classknncolle_1_1Searcher.html#a175ab05c339e3aa39b974f1ceebc21a3',1,'knncolle::Searcher::can_search_all()'],['https://knncolle.github.io/knncolle/classknncolle_1_1VptreeSearcher.html#a8df5ca5cd6c2f305da615f7839ba7894',1,'knncolle::VptreeSearcher::can_search_all()']]],
   ['cap_5fk_1',['cap_k',['https://knncolle.github.io/knncolle/namespaceknncolle.html#a926c81cd626de7081b073a891d352543',1,'knncolle']]],
-  ['cmake_20with_20tt_20fetchcontent_20tt_2',['CMake with &lt;tt&gt;FetchContent&lt;/tt&gt;',['../index.html#autotoc_md5',1,'']]],
-  ['cmake_20with_20tt_20find_5fpackage_20tt_3',['CMake with &lt;tt&gt;find_package()&lt;/tt&gt;',['../index.html#autotoc_md6',1,'']]],
-  ['count_5fall_5fneighbors_5fwithout_5fself_4',['count_all_neighbors_without_self',['https://knncolle.github.io/knncolle/namespaceknncolle.html#ab636bced615a3a669ef0e207b5d848cc',1,'knncolle']]],
-  ['create_5',['create',['../structknncolle__hnsw_1_1DistanceConfig.html#a58ad45a2489b1fb8617fe53f50add65e',1,'knncolle_hnsw::DistanceConfig']]],
-  ['customizing_20the_20search_6',['Customizing the search',['../index.html#autotoc_md3',1,'']]]
+  ['cap_5fk_5fquery_2',['cap_k_query',['https://knncolle.github.io/knncolle/namespaceknncolle.html#a918d96e82ead9e67d7c008bde1e22505',1,'knncolle']]],
+  ['cmake_20with_20tt_20fetchcontent_20tt_3',['CMake with &lt;tt&gt;FetchContent&lt;/tt&gt;',['../index.html#autotoc_md5',1,'']]],
+  ['cmake_20with_20tt_20find_5fpackage_20tt_4',['CMake with &lt;tt&gt;find_package()&lt;/tt&gt;',['../index.html#autotoc_md6',1,'']]],
+  ['count_5fall_5fneighbors_5fwithout_5fself_5',['count_all_neighbors_without_self',['https://knncolle.github.io/knncolle/namespaceknncolle.html#ab636bced615a3a669ef0e207b5d848cc',1,'knncolle']]],
+  ['create_6',['create',['../structknncolle__hnsw_1_1DistanceConfig.html#a58ad45a2489b1fb8617fe53f50add65e',1,'knncolle_hnsw::DistanceConfig']]],
+  ['customizing_20the_20search_7',['Customizing the search',['../index.html#autotoc_md3',1,'']]]
 ];
